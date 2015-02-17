76fe50b02373506d9eed1b0ab1dde602ad1f1488
Kenichi Tsunokawa <kenichi.tsunokawa@gmail.com>
2015-02-18 01:45:08 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Packer設定ファイル
+
+『Chef実践入門』サンプルコードを再利用させていただきました。  
+[wdpress/Chef_Practical_Guide · GitHub](https://github.com/wdpress/Chef_Practical_Guide)
+
+
+## ライセンス
+MIT License  
+http://opensource.org/licenses/mit-license.php
---
+++ centos/7/base.sh
@@ -0,0 +1,34 @@
+/usr/sbin/setenforce 0
+sudo sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+sudo sed -i "s/#UseDNS yes/UseDNS no/" /etc/ssh/sshd_config
+
+cat <<EOM | sudo tee -a /etc/yum.repos.d/epel.repo
+[epel]
+name=Extra Packages for Enterprise Linux 7 - $basearch
+#baseurl=http://download.fedoraproject.org/pub/epel/7/$basearch
+mirrorlist=https://mirrors.fedoraproject.org/metalink?repo=epel-7&arch=$basearch
+failovermethod=priority
+enabled=0
+gpgcheck=0
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7
+EOM
+
+sudo yum -y install gcc make automake autoconf libtool gcc-c++ kernel-headers-`uname -r` kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget nfs-utils bind-utils bzip2 git
+sudo yum -y --enablerepo=epel install dkms
+
+# Vagrant用の公開鍵を登録
+mkdir -pm 700 /home/vagrant/.ssh
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O /home/vagrant/.ssh/authorized_keys
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant /home/vagrant/.ssh
+
+cd /tmp
+sudo mount -o loop /home/vagrant/VBoxGuestAdditions.iso /mnt
+sudo sh /mnt/VBoxLinuxAdditions.run
+sudo umount /mnt
+
+sudo /etc/rc.d/init.d/vboxadd setup
+
+# オムニバスインストーラーを利用したChefのインストール
+curl -L https://www.opscode.com/chef/install.sh | sudo bash
+
