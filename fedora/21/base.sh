8081f0f0458eb676ec0ff525efabea7b7e66a581
Kenichi Tsunokawa <kenichi.tsunokawa@gmail.com>
2015-04-01 01:28:03 +0900

    Add fedora21

---
+++ fedora/21/base.sh
@@ -0,0 +1,22 @@
+/usr/sbin/setenforce 0
+sudo sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+sudo sed -i "s/#UseDNS yes/UseDNS no/" /etc/ssh/sshd_config
+
+sudo yum -y install gcc make automake autoconf libtool gcc-c++ kernel-headers-`uname -r` kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget nfs-utils bind-utils bzip2 git dkms
+
+# Vagrant用の公開鍵を登録
+mkdir -pm 700 /home/vagrant/.ssh
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O /home/vagrant/.ssh/authorized_keys
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant /home/vagrant/.ssh
+
+cd /tmp
+sudo mount -o loop /home/vagrant/VBoxGuestAdditions.iso /mnt
+sudo sh /mnt/VBoxLinuxAdditions.run
+sudo umount /mnt
+
+sudo /etc/rc.d/init.d/vboxadd setup
+
+# オムニバスインストーラーを利用したChefのインストール
+#curl -L https://www.opscode.com/chef/install.sh | sudo bash
+
